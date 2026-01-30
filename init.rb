128fe1d93dae19f8e5323428c2a0df5f74c4c19a
daoutech-rimichoi <rimichoi@daou.co.kr>
2026-01-30 09:47:54 +0900

    feat: redmine 5.x 지원

--- init.rb
+++ init.rb
@@ -2,8 +2,8 @@ Redmine::Plugin.register :redmine_status_report do
   name 'Redmine Status Report plugin'
   author 'rimichoi'
   description 'Shows status statistics for an issue'
-  version '1.0.0, Daoutech 0.0.1'
-  requires_redmine :version_or_higher => '6.0.0'
+  version '1.0.0, Daoutech 0.0.2'
+  requires_redmine :version_or_higher => '5.0.0'
   url 'https://github.com/daoutech-rimichoi/redmine_status_report'
   author_url 'mailto:rimichoi@daou.co.kr'
 end
